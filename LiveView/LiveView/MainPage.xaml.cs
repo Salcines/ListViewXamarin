c8c4b48f0c222788c13da7273d5e9f8f46183dbf
Francisco Salcines <fransalcines@hotmail.com>
2017-12-22 21:04:35 +0100

    Imagen incrustada en StackPanel

--- LiveView/LiveView/MainPage.xaml.cs
+++ LiveView/LiveView/MainPage.xaml.cs
@@ -7,12 +7,22 @@ using Xamarin.Forms;
 
 namespace LiveView
 {
-	public partial class MainPage : ContentPage
-	{
+    public partial class MainPage : ContentPage
+    {
 
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Cmdclick_OnClicked(object sender, EventArgs e)
+        {
+            lblName.Text = "@SalcinesFran";
+            lblName.BackgroundColor = Color.Blue;
+            lblName.TextColor = Color.White;
+            lblName.FontSize = 25;
+        }
+    }
 }
